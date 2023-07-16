543bdcff1edc7da52b26a74ea75b183dc1d49032
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-16 08:38:58 -0400

    Agregando el componente al modulo principal y al archivo de rutas general

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HomeComponent } from './home/home.component';
+import { UsersReservationsComponent } from './users-reservations/users-reservations.component';
 
 const routes: Routes = [
-  {path : '', component : HomeComponent },
+  { path : '', component : HomeComponent },
+  { path : 'users-reservations', component : UsersReservationsComponent },
   { path : '**', component : PagenotfoundComponent, pathMatch : 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { UsersReservationsComponent } from './users-reservations/users-reservations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    UsersReservationsComponent
   ],
   imports: [
     BrowserModule,
