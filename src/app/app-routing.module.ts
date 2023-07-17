b44fc4637147a164e7db0b3dced6cbbb7ddaa61b
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-16 22:06:04 -0400

    Creando la ruta al componente login y anexandolo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HomeComponent } from './home/home.component';
 import { UsersReservationsComponent } from './users-reservations/users-reservations.component';
 import { PageResponseComponent } from './users-reservations/components/page-response/page-response.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path : '', component : HomeComponent },
   { path : 'users-reservations', component : UsersReservationsComponent },
   { path : 'users-reservations/response', component : PageResponseComponent},
+  { path : 'login', component: LoginComponent },
   { path : '**', component : PagenotfoundComponent, pathMatch : 'full'}
 ];
 
