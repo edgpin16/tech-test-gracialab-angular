8fe4b4a58c49604e462580482bc2875819b5cdcf
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-17 20:43:28 -0400

    Agregando la logica del servicio para verificar jwt sea valido

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { StorageService } from './storage.service';
 
 const BASE_URL_AUTH = 'http://localhost:3001/api/auth/';
 
@@ -12,7 +13,10 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly storageService : StorageService
+  ) {}
 
   login(credentials : {email : string, password : string}): Observable<any> {
     return this.http.post(
@@ -20,5 +24,39 @@ export class AuthService {
       credentials,
       httpOptions
     );
+  }
+
+  public verifyJWT() : boolean {
+    const access_token : string = this.storageService.getJWT();
+
+    const newHttpOptions = {
+      headers : new HttpHeaders({ 'Content-Type': 'application/json' }).set('Authorization', `Bearer ${access_token}`)
+    }
+
+    const response = this.http.post(
+      BASE_URL_AUTH + 'verify-jwt',
+      {},
+      newHttpOptions
+    ).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: ( error ) => {
+        const {message, name} = error;
+        console.log({
+          message,
+          name,
+          error
+        });
+        }
+      });
+
+      //Puede ser que me devuelva un boolean true si el token es valido
+      //O un observable si el token es invalido
+      //Por eso si es un boolean, es 100% que sea true
+      //ya que en caso de error, no devuelve un boolean
+      return typeof response === 'boolean';
+      
+      //if(typeof response === 'boolean') return true; return false;
   }
 }
